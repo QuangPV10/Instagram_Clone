fd6e32781faa29a84cff2b5d9d2aef31f7c4c95f
Quang Pham <phamvanquang.work@gmail.com>
2023-03-29 17:49:38 +0700

    fix bug search feature

--- lib/src/screens/search_screen.dart
+++ lib/src/screens/search_screen.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone/src/share/constant.dart';
+
+import '../utils/global_variable.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -15,45 +18,78 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: TextFormField(
-          controller: searchController,
-          decoration: const InputDecoration(hintText: 'Search for a User'),
-          onFieldSubmitted: (value) {
-            setState(() {
-              isShowUser = true;
-            });
-          },
-        ),
-      ),
-      body: isShowUser
-          ? FutureBuilder(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .where('username', isEqualTo: searchController.text)
-                  .get(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: Form(
+            child: TextFormField(
+              controller: searchController,
+              decoration: const InputDecoration(hintText: 'Search for a User'),
+              onFieldSubmitted: (value) {
+                setState(() {
+                  isShowUser = true;
+                });
+
+                if (value.isEmpty) {
+                  setState(() {
+                    isShowUser = false;
+                  });
                 }
-                return ListView.builder(
-                  itemCount: (snapshot.data! as dynamic).docs.length,
-                  itemBuilder: (context, index) {
-                    final user = (snapshot.data! as dynamic).docs[index];
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(user['photoUrl']),
-                      ),
-                      title: Text(user['username']),
-                    );
-                  },
-                );
               },
-            )
-          : Container(),
-    );
+            ),
+          ),
+        ),
+        body: isShowUser
+            ? FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .where('username', isGreaterThanOrEqualTo: searchController.text)
+                    .get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) {
+                      final user = (snapshot.data! as dynamic).docs[index];
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(user['photoUrl']),
+                        ),
+                        title: Text(user['username']),
+                      );
+                    },
+                  );
+                },
+              )
+            : Container()
+
+        // FutureBuilder(
+        //     future: FirebaseFirestore.instance.collection('posts').get(),
+        //     builder: (context, snapshot) {
+        //       if (!snapshot.hasData) {
+        //         return const Center(
+        //           child: CircularProgressIndicator(),
+        //         );
+        //       }
+
+        //       return StaggeredGridView.countBuilder(
+        //         crossAxisCount: 3,
+        //         itemCount: (snapshot.data! as dynamic).docs.length,
+        //         itemBuilder: (context, index) => Image.network(
+        //           (snapshot.data! as dynamic).docs[index]['postUrl'],
+        //           fit: BoxFit.cover,
+        //         ),
+        //         staggeredTileBuilder: (index) => MediaQuery.of(context).size.width > webScreenSize
+        //             ? StaggeredTile.count((index % 7 == 0) ? 1 : 1, (index % 7 == 0) ? 1 : 1)
+        //             : StaggeredTile.count((index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1),
+        //         mainAxisSpacing: 8.0,
+        //         crossAxisSpacing: 8.0,
+        //       );
+        //     },
+        //   ),
+        );
   }
 }
