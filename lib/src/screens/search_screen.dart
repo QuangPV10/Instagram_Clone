e9e96da86a4ee517fde95a3011ce5e4b14074df6
Quang Pham <phamvanquang.work@gmail.com>
2023-03-29 17:35:18 +0700

    search users

--- lib/src/screens/search_screen.dart
+++ lib/src/screens/search_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/src/share/constant.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,13 +10,50 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final searchController = TextEditingController();
+  bool isShowUser = false;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        icon: const Icon(Icons.upload),
-        onPressed: () {},
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+          decoration: const InputDecoration(hintText: 'Search for a User'),
+          onFieldSubmitted: (value) {
+            setState(() {
+              isShowUser = true;
+            });
+          },
+        ),
       ),
+      body: isShowUser
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('username', isEqualTo: searchController.text)
+                  .get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) {
+                    final user = (snapshot.data! as dynamic).docs[index];
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(user['photoUrl']),
+                      ),
+                      title: Text(user['username']),
+                    );
+                  },
+                );
+              },
+            )
+          : Container(),
     );
   }
 }
